6e17efd49a25e62c6c3fcaf0201e710ad3327aaa
Yuliana Smirnova <0387097@gmail.com>
2021-06-20 11:20:17 +0300

    Classes Movie, Manager, Test added

---
+++ src/main/java/ru/netology/domain/Movie.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class Movie {
+    private int id;
+    private String name;
+    private String imgUrl;
+    private boolean premiereTomorrow;
+}
